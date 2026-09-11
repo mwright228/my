d330263780e2f66270601f845573c925b3cd94ee
mwright228 <mwright228@users.noreply.github.com>
2026-09-11 13:54:33 +0500

    fix(build): add compose material-icons-extended dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.kotlinx.coroutines.android)
 
     debugImplementation(libs.androidx.ui.tooling)
